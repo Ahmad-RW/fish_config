dd5f1d6f197cf921006f02d23a55ee5a69f319d0
Ahmad-RW <gearsveteran@outlook.com>
2023-06-22 12:43:40 +0300

    aliases to switch between stg and prod

--- config.fish
+++ config.fish
@@ -1,6 +1,13 @@
 alias l "ls -ltr"
 alias .kube "cd ~/.kube"
 alias .oci "cd ~/.oci"
+alias .fish "cd ~/.config/fish"
+alias awsstg "source ~/.config/fish/init_aws_staging.sh"
+alias awsprod "source ~/.config/fish/init_aws_production.sh"
+ 
+
+
+
 export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING=True
 
 if status is-interactive
