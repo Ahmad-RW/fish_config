27ea5c707c9f78023b92813798b3b4c1fcfc890d
Ahmad Rw <ahmadrw@Ahmads-MacBook-Air.local>
2023-06-22 11:30:41 +0300

    init fish config

---
+++ config.fish
@@ -0,0 +1,8 @@
+alias l "ls -ltr"
+alias .kube "cd ~/.kube"
+alias .oci "cd ~/.oci"
+export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING=True
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
